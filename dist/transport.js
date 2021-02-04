1836a2ec4d74d026355e9968f77f674331d96cb3
Jeff Mikels <jeffweb@mikels.cc>
2021-02-04 12:13:13 -0500

    still trying to configure for npm

--- dist/transport.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ViscaTransport = void 0;
-class ViscaTransport {
-    write(s) { }
-}
-exports.ViscaTransport = ViscaTransport;
-//# sourceMappingURL=transport.js.map
